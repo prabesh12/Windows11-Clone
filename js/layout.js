894e90d44f7e9021301393bdc85d2b5bad0c000a
PrabeshOli <herokaje886@gmail.com>
2022-01-07 20:39:09 +0545

    start-menu

--- js/layout.js
+++ js/layout.js
@@ -1,23 +1,113 @@
 let body = document.querySelector("body");
-
-
 let startbtn = document.getElementsByClassName("windowbtn")[0];
+let iconsDiv = document.createElement("div");
+let searchDiv = document.createElement("div");
+let searchField = document.createElement("input")
+let powerDiv = document.createElement("div");
 let startmenu = document.createElement("div");
+let powerOff = document.createElement("div");
 startmenu.className = "startmenu";
 startmenu.style.top = "125%";
-
-let powerOff = document.createElement("div");
+iconsDiv.className = "icons-div"
+searchDiv.className="search-div"
+searchField.className= "search-field"
+searchField.placeholder="Type here to search"
+powerDiv.className="power-div";
 powerOff.className = "powerOff";
 powerOff.style.height = "30px";
 powerOff.style.width = "30px";
+document.body.appendChild(startmenu);
+powerDiv.appendChild(powerOff);
+searchDiv.appendChild(searchField);
+startmenu.append(searchDiv, iconsDiv, powerDiv);
 // powerOff.style.backgroundImage = "url(../img/shutdown.png)";
 
-document.body.appendChild(startmenu);
-startmenu.appendChild(powerOff);
+// windows icons list starts here
+
+const iconList = [
+  { id: 1, src: "img/icons8-microsoft-edge-48.png", name: "edge" },
+  { id: 2, src: "img/icons8-microsoft-word-2019-50.png", name: "Word" },
+  {
+    id: 3,
+    src: "img/icons8-microsoft-excel-2019-50.png",
+    name: "Excel",
+  },
+  {
+    id: 4,
+    src: "img/icons8-mail-50.png",
+    name: "Mail",
+  },
+  {
+    id: 5,
+    src: "img/icons8-calendar-50.png",
+    name: "Calendar",
+  },
+  {
+    id: 6,
+    src: "img/icons8-microsoft-store-50.png",
+    name: "Miscrosoft Store",
+  },
+  {
+    id: 7,
+    src: "img/icons8-photos-50.png",
+    name: "Photos",
+  },
+  {
+    id: 8,
+    src: "img/icons8-settings-50.png",
+    name: "Setting",
+  },
+  {
+    id: 9,
+    src: "img/icons8-microsoft-office-2019-48.png",
+    name: "Office",
+  },
+  {
+    id: 10,
+    src: "img/icons8-microsoft-solitaire-collection-50.png",
+    name: "Solitaire",
+  },
+  {
+    id: 11,
+    src: "img/icons8-spotify-50.png",
+    name: "Spotify",
+  },
+  {
+    id: 12,
+    src: "img/icons8-whatsapp-48.png",
+    name: "WhatsAPp",
+  },
+  {
+    id: 13,
+    src: "img/icons8-microsoft-todo-2019-50.png",
+    name: "Todo",
+  },
+  {
+    id: 14,
+    src: "img/icons8-xbox-x-48.png",
+    name: "Xbox",
+  },
+  {
+    id: 15,
+    src: "img/icons8-amazon-prime-video-48.png",
+    name: "Prime Video",
+  },
+  {
+    id: 16,
+    src: "img/icons8-tiktok-48.png",
+    name: "Tiktok",
+  },
+];
+
+// windows icons list ends here
 
 startbtn.addEventListener("click", () =>{
     if(startmenu.style.top == '125%'){
-        startmenu.style.top = '5%';
+        if(window.innerHeight >= 655){
+        startmenu.style.top = '15%';
+        }else{
+            startmenu.style.top = '4.8%';
+        }
         startbtn.style.color = "rgb(14,13,13)";
         startbtn.style.boxShadow = "0 5px 15px rgba(15,125,228,0.5)";
     //     color: rgb(14, 13, 13);
@@ -29,63 +119,55 @@ startbtn.addEventListener("click", () =>{
   
 })
 
-shutdown = function(){
-    body.style.background = "black";
-    body.style.filter = "brightness(0%)";
-
-}
-
-powerOff.addEventListener("click",shutdown);
-
-
-
+iconList.forEach((element) => {
+  console.log(element);
+  let windowsdiv = document.createElement("div");
+  let windowsImg = document.createElement("img");
+  windowsdiv.className="windows-div"
+  windowsImg.className="windows-icons"
+  windowsImg.src = element.src;
+  console.log(windowsImg.src);
+  windowsdiv.appendChild(windowsImg);
+  windowsdiv.setAttribute("id", `${element.id}`);
+  iconsDiv.append(windowsdiv);
+});
+shutdown = function () {
+  body.style.background = "black";
+  body.style.filter = "brightness(0%)";
+};
+powerOff.addEventListener("click", shutdown);
 let utilsbtn = document.getElementsByClassName("utils")[0];
 let utilsdiv = document.createElement("div");
 utilsdiv.className = "utilsDiv";
 utilsdiv.style.left = "25%";
-
 let bSlider = document.createElement("div");
 bSlider.className = "bSlider";
-
-
 let brightnessIcon = document.createElement("img");
-brightnessIcon.src = "../img/brightness.png";
+brightnessIcon.src = "img/brightness.png";
 let brange = document.createElement("input");
 brange.type = "range";
 brange.id = "brightness-range";
-brange.min ="0";
-brange.max="100";
-brange.value ="100";
-brange.onchange = function(){brightness(brange)};
-
-
+brange.min = "0";
+brange.max = "100";
+brange.value = "100";
+brange.onchange = function () {
+  brightness(brange);
+};
 
 bSlider.appendChild(brange);
 bSlider.appendChild(brightnessIcon);
 utilsdiv.appendChild(bSlider);
 
-
-
-
 document.body.appendChild(utilsdiv);
 
-
-
-utilsbtn.addEventListener("click", () =>{
-    console.log(utilsdiv.style.left);
-    if(utilsdiv.style.left == "25%"){
-        
-        utilsdiv.style.left = "-1%";
-        console.log("left move");   
-        
-    }else{
-        utilsdiv.style.left = "25%";
-        
-        
-    }
-  
-})
-
+utilsbtn.addEventListener("click", () => {
+  console.log(utilsdiv.style.left);
+  if(window.innerHeight >= 655){
+    utilsdiv.style.bottom = "16vh";
+}else{
+    utilsdiv.style.bottom = "32vh";
+}
+});
 
 let folder = document.getElementsByClassName("folder")[0];
 folder.style.width = "100%";
@@ -93,29 +175,20 @@ let mypc = document.getElementsByClassName("mypc")[0];
 let close = document.getElementsByClassName("close")[0];
 let maximize = document.getElementsByClassName("maximize")[0];
 
-mypc.addEventListener("dblclick",()=>{
-    folder.style.display = "block";
-})
+mypc.addEventListener("dblclick", () => {
+  folder.style.display = "block";
+});
 
-close.addEventListener("click",()=>{
-    folder.style.display = "none";
-})
+close.addEventListener("click", () => {
+  folder.style.display = "none";
+});
 
-maximize.addEventListener("click",()=>{
-    if(folder.style.width == "100%"){
+maximize.addEventListener("click", () => {
+  if (folder.style.width == "100%") {
     folder.style.width = "50%";
     folder.style.height = "60%";
-}else{
+  } else {
     folder.style.width = "100%";
     folder.style.height = "105.4%";
-}
-
-})
-
-        
-
-
-
-
-
-
+  }
+});
---
+++ js/list.js
@@ -0,0 +1,68 @@
+//  const iconList = {
+//    Icons: [
+//             {
+//                 src: "img/icons8-microsoft-edge-48.png",
+//                 name: "edge"
+//             },
+//             {
+//                 src: "img/icons8-microsoft-word-2019.png",
+//                 name: "Word"
+//             },
+//             {
+//                 src: "img/icons8-microsoft-excel-2019-50.png",
+//                 name: "Excel"
+//             },
+//             {
+//                 src: "img/icons8-mail-50.png",
+//                 name: "Mail"
+//             },
+//             {
+//                 src: "img/icons8-calendar-50.png",
+//                 name: "Calendar"
+//             },
+//             {
+//                 src: "img/icons8-microsoft-store-50.png",
+//                 name: "Miscrosoft Store"
+//             },
+//             {
+//                 src: "img/icons8-photos-50.png",
+//                 name: "Photos"
+//             },
+//             {
+//                 src: "img/icons8-settings-50.png",
+//                 name: "Setting"
+//             },
+//             {
+//                 src: "img/img\icons8-microsoft-office-2019-48.png",
+//                 name: "Office"
+//             },
+//             {
+//                 src: "img/icons8-microsoft-solitaire-collection-50.png",
+//                 name: "Solitaire"
+//             },
+//             {
+//                 src: "img/icons8-spotify-50.png",
+//                 name: "Spotify"
+//             },
+//             {
+//                 src: "img/icons8-whatsapp-48.png",
+//                 name: "WhatsAPp"
+//             },
+//             {
+//                 src: "img/icons8-microsoft-todo-2019-50.png",
+//                 name: "Todo"
+//             },
+//             {
+//                 src: "img/icons8-xbox-x-48.png",
+//                 name: "Xbox"
+//             },
+//             {
+//                 src: "img/icons8-amazon-prime-video-48.png",
+//                 name: "Prime Video"
+//             },
+//             {
+//                 src: "img/icons8-tiktok-48.png",
+//                 name: "Tiktok"
+//             },
+//     ]
+// }
